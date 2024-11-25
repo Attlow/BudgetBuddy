756667a8015f644ce5a7567967d70c74be3a5b48
Attlow <vinicius.alexander2004@gmail.com>
2024-11-24 22:20:38 -0300

    0.2

--- lib/budget_web/controllers/user_session_controller.ex
+++ lib/budget_web/controllers/user_session_controller.ex
@@ -4,9 +4,8 @@ defmodule BudgetWeb.UserSessionController do
   alias Budget.Accounts
   alias BudgetWeb.UserAuth
 
-  # Adapta os casos com "_action"
-  def create(conn, %{"_action" => _action} = params) do
-    create(conn, params["user"], "Account created successfully!")
+  def create(conn, %{"_action" => "registered"} = params) do
+    create(conn, params, "Account created successfully!")
   end
 
   def create(conn, %{"_action" => "password_updated"} = params) do
@@ -15,32 +14,26 @@ defmodule BudgetWeb.UserSessionController do
     |> create(params, "Password updated successfully!")
   end
 
-  # Rota genérica de login
   def create(conn, params) do
     create(conn, params, "Welcome back!")
   end
 
-  defp create(conn, %{"user" => %{"login" => login, "password" => password}}, info) do
-    # Determina se é login por email ou nome
-    user =
-      if String.contains?(login, "@") do
-        Accounts.get_user_by_email_and_password(login, password)
-      else
-        Accounts.get_user_by_name_and_password(login, password)
-      end
+  defp create(conn, %{"user" => user_params}, info) do
+    %{"email" => email, "password" => password} = user_params
 
-    if user do
+    if user = Accounts.get_user_by_email_and_password(email, password) do
       conn
       |> put_flash(:info, info)
-      |> UserAuth.log_in_user(user)
+      |> UserAuth.log_in_user(user, user_params)
+
     else
+      # In order to prevent user enumeration attacks, don't disclose whether the email is registered.
       conn
-      |> put_flash(:error, "Invalid username/email or password")
-      |> put_flash(:login, String.slice(login, 0, 160))
+      |> put_flash(:error, "Invalid email or password")
+      |> put_flash(:email, String.slice(email, 0, 160))
       |> redirect(to: ~p"/users/log_in")
     end
   end
-
 
   def delete(conn, _params) do
     conn
